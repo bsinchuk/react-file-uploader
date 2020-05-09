afacdcc73c4709b491ab61d5b7bd840922e8c8fc
Bogdan Sinchuk <bogdancse@gmail.com>
2020-05-09 13:28:38 +0300

    backend upload route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const app = express();
+const formidable = require('formidable');
 
+const app = express();
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
@@ -11,3 +12,28 @@ app.get('/test', (req, res) => {
   res.send({status: 'all is ok'});
 });
 
+app.post('/upload', (req, res) => {
+  const form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.multiples = true;
+  form.uploadDir = __dirname + '/public/uploads';
+  form.on('file', (name, file) => {
+    console.log('Uploaded file: ', name);
+  });
+  form.on('fileBegin', function (name, file){
+    console.log('fileBegin starts: ', name);
+    file.path = form.uploadDir + '/' + file.name
+  })
+
+  form.on('aborted', () => {
+    console.error('Request aborted by the user')
+  });
+  form.on('error', (err) => {
+    console.error('Error', err)
+  });
+  form.on('end', () => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.end('success');
+  });
+  form.parse(req);
+});
